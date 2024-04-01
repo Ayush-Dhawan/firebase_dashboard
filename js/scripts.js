af0260f9e08d926fc7c5c4fcaaa34800defab65c
Ayush-Dhawan <122392703+Ayush-Dhawan@users.noreply.github.com>
2024-04-01 23:18:42 +0530

    Update scripts.js

--- js/scripts.js
+++ js/scripts.js
@@ -4,7 +4,7 @@ let sidebarOpen = false;
 const sidebar = document.getElementById('sidebar');
 
 function openSidebar() {
-  if (!sidebarOpen) {
+  if (!sidebarOpen) { 
     sidebar.classList.add('sidebar-responsive');
     sidebarOpen = true;
   }
