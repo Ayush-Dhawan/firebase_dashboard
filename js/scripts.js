2d1888ed487491a47d62dd4ff829c4d9f47e5db2
Ayush-Dhawan <122392703+Ayush-Dhawan@users.noreply.github.com>
2025-02-18 11:01:01 +0530

    Update scripts.js

--- js/scripts.js
+++ js/scripts.js
@@ -2,9 +2,9 @@ import React, { createContext, useContext, useReducer } from "react";
 
 // Create Cart Context const CartContext = createContext();
 
-// Initial state const initialState = { cart: [], };
+// Initial state const initialState = { carts: {}, };
 
-// Reducer function const cartReducer = (state, action) => { switch (action.type) { case "ADD_TO_CART": return { ...state, cart: [...state.cart, action.payload] }; case "REMOVE_FROM_CART": return { ...state, cart: state.cart.filter((item) => item.id !== action.payload), }; case "CLEAR_CART": return { ...state, cart: [] }; default: return state; } };
+// Reducer function const cartReducer = (state, action) => { switch (action.type) { case "ADD_TO_CART": return { ...state, carts: { ...state.carts, [action.username]: [...(state.carts[action.username] || []), action.payload], }, }; case "REMOVE_FROM_CART": return { ...state, carts: { ...state.carts, [action.username]: state.carts[action.username]?.filter((item) => item.id !== action.payload) || [], }, }; case "CLEAR_CART": return { ...state, carts: { ...state.carts, [action.username]: [], }, }; default: return state; } };
 
 // CartProvider component export const CartProvider = ({ children }) => { const [state, dispatch] = useReducer(cartReducer, initialState);
 
