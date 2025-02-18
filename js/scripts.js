d3015d2fb06f7b4badf5c3266e3b02c9fa395e91
Ayush-Dhawan <122392703+Ayush-Dhawan@users.noreply.github.com>
2025-02-18 15:33:40 +0530

    Update scripts.js

--- js/scripts.js
+++ js/scripts.js
@@ -1,14 +1,11 @@
-useEffect(() => {
-  // Check if the customer has any items in the cart
-  const customerCart = state.carts[customer?.id] || [];
-
-  // Find the item with the matching rewardId
-  const itemInCart = customerCart.find(item => item.item.id === rewardId);
-
-  // If item is found, set the initial quantity; otherwise, set it to 0
-  if (itemInCart) {
-    setCartQuantity(itemInCart.item.quantity);
-  } else {
-    setCartQuantity(0);
-  }
-}, [rewardId, state.carts, customer?.id]);
+function CartItem({item}) {
+console.log(item)
+  return(
+    <div>
+      <span>{item.itemName}</span>
+      <span>{item.itemCost}</span>
+      <span>{item.quantity}</span>
+      <span>{item.status}</span>
+    </div>
+  )
+}
