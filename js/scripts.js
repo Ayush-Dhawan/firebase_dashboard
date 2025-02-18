fad3b5dc1e685c419351074a1546b73848073a91
Ayush-Dhawan <122392703+Ayush-Dhawan@users.noreply.github.com>
2025-02-18 14:37:36 +0530

    Update scripts.js

--- js/scripts.js
+++ js/scripts.js
@@ -1,14 +1,12 @@
 case "UPDATE_CART_QUANTITY":
-      return {
-        ...state,
-        carts: {
-          ...state.carts,
-          [action.id]: state.carts[action.id].map((item) =>
-            item.id === action.payload.id
-              ? { ...item, quantity: action.payload.quantity }
-              : item
-          ),
-        },
-      };
-
-now modify this accordingly to update quantity field inside the rewardItem
+  return {
+    ...state,
+    carts: {
+      ...state.carts,
+      [action.id]: state.carts[action.id].map((item) =>
+        item.id === action.payload.rewardId  // Compare by rewardId
+          ? { ...item, quantity: action.payload.quantity }  // Update the quantity of the rewardItem
+          : item
+      ),
+    },
+  };
