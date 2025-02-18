86690ae3c23e2d2da32794564fcbd0b57b18e9f5
Ayush-Dhawan <122392703+Ayush-Dhawan@users.noreply.github.com>
2025-02-18 15:17:18 +0530

    Update scripts.js

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,14 @@
-  const [cartQuantity, setCartQuantity] = useState(0);
+useEffect(() => {
+  // Check if the customer has any items in the cart
+  const customerCart = state.carts[customer?.id] || [];
 
-  console.log("ye hai carts: ", state.carts[customer?.id])
+  // Find the item with the matching rewardId
+  const itemInCart = customerCart.find(item => item.item.id === rewardId);
 
-this takes us to the cart of the user, now using rewardId you need to check which item we are on based on item.id and then use that items.quantity attribute to set initial state of cartQuantity
+  // If item is found, set the initial quantity; otherwise, set it to 0
+  if (itemInCart) {
+    setCartQuantity(itemInCart.item.quantity);
+  } else {
+    setCartQuantity(0);
+  }
+}, [rewardId, state.carts, customer?.id]);
