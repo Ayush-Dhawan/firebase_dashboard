03d304189faa8ad04c642deefaf98d4ae78074fb
Ayush-Dhawan <122392703+Ayush-Dhawan@users.noreply.github.com>
2025-02-19 10:45:08 +0530

    Update scripts.js

--- js/scripts.js
+++ js/scripts.js
@@ -1,9 +1,74 @@
-useEffect(() => {
-  setItems(state.carts[customer.id] || []);
+import React, { createContext, useContext, useReducer } from 'react';
 
-  const cartItems = state.carts[customer.id] || [];
+const CartContext = createContext(null);
 
-  setTotalQuantity(cartItems.reduce((sum, item) => sum + Number(item.item.quantity), 0));
-  setTotalPrice(cartItems.reduce((sum, item) => sum + Number(item.item.quantity) * Number(item.item.itemCost), 0));
+const initialState = {
+  // carts: { customerId: [ { item: { ...reward, quantity } }, ... ] }
+  carts: {}
+};
 
-}, [state.carts, customer.id]);
+const cartReducer = (state, action) => {
+  switch (action.type) {
+    case 'ADD_TO_CART': {
+      // action.payload: { item: rewardWithQuantity }
+      return {
+        ...state,
+        carts: {
+          ...state.carts,
+          [action.id]: [...(state.carts[action.id] || []), action.payload],
+        },
+      };
+    }
+    case "UPDATE_CART_QUANTITY":
+      return {
+        ...state,
+        carts: {
+          ...state.carts,
+          [action.id]: state.carts[action.id].map((item) =>
+            item.item.id === action.payload.id
+              ? { ...item, item: { ...item.item, quantity: action.payload.quantity } }
+              : item
+          ),
+        },
+      }
+    case 'REMOVE_FROM_CART': {
+      return {
+        ...state,
+        carts: {
+          ...state.carts,
+          [action.id]:
+          state.carts[action.id]?.filter((cartItem) => cartItem.item.id !== action.payload) ||
+          [],
+        },
+      };
+    }
+    case 'CLEAR_CART': {
+      return {
+        ...state,
+        carts: {
+          ...state.carts,
+          [action.id]: [],
+        },
+      };
+    }
+    default:
+      return state;
+  }
+};
+
+export const CartProvider = ({ children }) => {
+  const [state, dispatch] = useReducer(cartReducer, initialState);
+
+  return (
+    <CartContext.Provider value={{ state, dispatch }}>
+      {children}
+    </CartContext.Provider>
+  );
+};
+
+export const useCart = () => useContext(CartContext);
+    TypeError: Cannot destructure property 'state' of '((cov_s90ywmcz3(...).s[9]++) , (0 , _CartContext.useCart)(...))' as it is null.
+ 
+      28 |   const [pointsCost, setPointsCost] = useState(0);
+      29 |   const [isProcessing, setIsProcessing] = useState(false);
+> 30 |   const {state, dispatch} = useCart();
