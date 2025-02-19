4852a578445c13080ad1abb3a179f6ba5c3dd250
Ayush-Dhawan <122392703+Ayush-Dhawan@users.noreply.github.com>
2025-02-19 21:32:00 +0530

    Update scripts.js

--- js/scripts.js
+++ js/scripts.js
@@ -1,23 +1,30 @@
-useEffect(() => {
-  const fetchRedemptionCounts = async () => {
-    try {
-      if (!customer?.id) return; // Ensure customer ID exists
+package com.jpmorgan.et.mapper;
 
-      const fulfillments = await getFulfillments(customer.id);
-      console.log("Fulfillments: ", fulfillments);
+import com.jpmorgan.et.model.Fulfillment;
+import com.jpmorgan.et.model.FulfillmentByCustomer;
+import com.jpmorgan.et.model.FulfillmentFilter;
+import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Param;
 
-      const counts = fulfillments?.reduce((acc, fulfillment) => {
-        if (fulfillment.reward_catalog_id) {
-          acc[fulfillment.reward_catalog_id] = (acc[fulfillment.reward_catalog_id] || 0) + fulfillment.qty;
-        }
-        return acc;
-      }, {});
+import java.time.ZonedDateTime;
+import java.util.List;
+import java.util.Map;
 
-      setRedemptionCounts(counts);
-    } catch (error) {
-      console.error("Error fetching redemption counts:", error);
-    }
-  };
+/* TODO: Story #0.6
+    - Note the use of the @Mapper MyBatis annotation
+    - MyBatis will generate an implementation class for this interface
+    - MyBatis will make the implementation class a Spring Bean
+*/
+@Mapper
+public interface FulfillmentMapper {
 
-  fetchRedemptionCounts();
-}, [customer]);
+    List<FulfillmentByCustomer> getFulfillmentByCustomerId(final Long customerId);
+    List<FulfillmentByCustomer> getFulfillments();
+    void addFulfillment(Fulfillment fulfillment);
+
+    List<FulfillmentFilter> getFulfillmentFilteredData(
+            @Param("stDate") ZonedDateTime stDate,
+            @Param("endDate") ZonedDateTime endDate
+    );
+
+}
