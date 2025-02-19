80033335af77e9d964618a3c1d7972b98b410255
Ayush-Dhawan <122392703+Ayush-Dhawan@users.noreply.github.com>
2025-02-19 13:11:43 +0530

    Update scripts.js

--- js/scripts.js
+++ js/scripts.js
@@ -1,74 +1,222 @@
-import React, { createContext, useContext, useReducer } from 'react';
+import React, { useEffect, useState } from 'react';
+import { getRewardsList } from '../../api/RewardsApi';
+import { Link, useNavigate, useParams } from 'react-router-dom';
+import RewardsCard from './RewardsCard';
+import SearchBar from '../common/SearchBar';
+import { Button } from 'reactstrap';
 
-const CartContext = createContext(null);
+const RewardsSelectionPage = () => {
+  const [rewardItems, setRewardItems] = useState([]);
+  const [isLoading, setIsLoading] = useState(false);
+  const { categoryId } = useParams();
 
-const initialState = {
-  // carts: { customerId: [ { item: { ...reward, quantity } }, ... ] }
-  carts: {}
-};
+  const [allData, setAllData] = useState([]);
+  const [isActive, setIsActive] = useState(false);
+  const [isInactive, setIsInactive] = useState(false);
+  const [isSale, setIsSale] = useState(false);
+  const [isAll, setIsAll] = useState(true);
+  const [count, setCount] = useState(0);
+  const [searchQuery, setSearchQuery] = useState('');
 
-const cartReducer = (state, action) => {
-  switch (action.type) {
-    case 'ADD_TO_CART': {
-      // action.payload: { item: rewardWithQuantity }
-      return {
-        ...state,
-        carts: {
-          ...state.carts,
-          [action.id]: [...(state.carts[action.id] || []), action.payload],
-        },
-      };
+  const navigate = useNavigate();
+
+  const updateCount = (num) => {
+    setCount(count + num);
+  };
+
+  useEffect(() => {
+    setIsLoading(true);
+    getRewardsList(categoryId).then((response) => {
+      setRewardItems(response);
+      setIsLoading(false);
+      setAllData(response);
+    });
+  }, [categoryId]);
+
+  useEffect(() => {
+    let filteredItems = allData;
+
+    if (!isAll) {
+      filteredItems = filteredItems.filter((x) => {
+        return (
+          (x.status === 'Active' && isActive) ||
+          (x.status === 'Inactive' && isInactive) ||
+          (x.status === 'sale' && isSale)
+        );
+      });
     }
-    case "UPDATE_CART_QUANTITY":
-      return {
-        ...state,
-        carts: {
-          ...state.carts,
-          [action.id]: state.carts[action.id].map((item) =>
-            item.item.id === action.payload.id
-              ? { ...item, item: { ...item.item, quantity: action.payload.quantity } }
-              : item
-          ),
-        },
-      }
-    case 'REMOVE_FROM_CART': {
-      return {
-        ...state,
-        carts: {
-          ...state.carts,
-          [action.id]:
-          state.carts[action.id]?.filter((cartItem) => cartItem.item.id !== action.payload) ||
-          [],
-        },
-      };
-    }
-    case 'CLEAR_CART': {
-      return {
-        ...state,
-        carts: {
-          ...state.carts,
-          [action.id]: [],
-        },
-      };
-    }
-    default:
-      return state;
-  }
-};
 
-export const CartProvider = ({ children }) => {
-  const [state, dispatch] = useReducer(cartReducer, initialState);
+    if (searchQuery) {
+      filteredItems = filteredItems.filter((item) =>
+        item.itemName && item.itemName.toLowerCase().includes(searchQuery.toLowerCase())
+      );
+    }
+
+    setRewardItems(filteredItems);
+  }, [isActive, isInactive, isSale, isAll, searchQuery, allData]);
+
+  const handleSearch = (query) => {
+    setSearchQuery(query);
+  };
 
   return (
-    <CartContext.Provider value={{ state, dispatch }}>
-      {children}
-    </CartContext.Provider>
+    <main>
+      <nav aria-label="breadcrumb" role="navigation">
+        <ol className="breadcrumb">
+          <li className="breadcrumb-item">
+            <Link to="/">Home</Link>
+          </li>
+          <li className="breadcrumb-item">
+            <Link to="/category">Reward Categories</Link>
+          </li>
+        </ol>
+
+
+        {categoryId === '2' && (
+          <div>
+            <Button onClick={() => navigate("selectDates")}>
+              Search for Travel between specified dates
+            </Button>
+          </div>
+        )}
+
+        <div style={{ width: "100%", paddingBottom: "10px" }}>
+          <SearchBar onSearch={handleSearch} />
+        </div>
+
+        <div
+          style={{
+            paddingTop: '10px',
+            display: 'flex',
+            width: '100%',
+            justifyContent: 'space-around',
+          }}
+        >
+          <div>
+            <button
+              className={`px-4 py-2 ${isAll ? 'bg-black text-white' : ''}`}
+              onClick={() => setIsAll(!isAll)}
+            >
+              Show All Items
+            </button>
+          </div>
+
+          <div>
+            <button
+              className={`px-4 py-2 ${isActive ? 'bg-black text-white' : ''}`}
+              onClick={() => {
+                setIsAll(false);
+                setIsActive(!isActive);
+              }}
+            >
+              Show Active
+            </button>
+          </div>
+          <div>
+            <button
+              className={`px-4 py-2 ${isInactive ? 'bg-black text-white' : ''}`}
+              onClick={() => {
+                setIsAll(false);
+                setIsInactive(!isInactive);
+              }}
+            >
+              Show Inactive
+            </button>
+          </div>
+          <div>
+            <button
+              className={`px-4 py-2 ${isSale ? 'bg-black text-white' : ''}`}
+              onClick={() => {
+                setIsAll(false);
+                setIsSale(!isSale);
+              }}
+            >
+              Show Sale
+            </button>
+
+          </div>
+          <div style={{margin: "20px 0", textAlign: "center"}}>
+            <label htmlFor="sort">Sort By:</label>
+            <select
+              id="sort"
+
+              style={{padding: "5px", borderRadius: "5px"}}
+            >
+              <option value="A-Z">A-Z</option>
+              <option value="Z-A">Z-A</option>
+              <option value="Most Expensive">Most Expensive</option>
+              <option value="Least Expensive">Least Expensive</option>
+              <option value="Most Redeemed">Most Redeemed</option>
+              <option value="Least Redeemed">Least Redeemed</option>
+
+            </select>
+          </div>
+        </div>
+      </nav>
+      {isLoading ? (
+        <p>Loading...</p>
+      ) : (
+        <>
+          {rewardItems.length > 0 ? (
+            <>
+              <p>Overall count: {count}</p>
+              <div className="cards">
+                {rewardItems.map((item) => (
+                  <div className="cards-item" key={item.id}>
+                    <RewardsCard
+                      isUpdatable={true}
+                      reward={item}
+                      updateCount={updateCount}
+                      showPin="true"
+                    />
+                  </div>
+                ))}
+              </div>
+            </>
+          ) : (
+            <div
+              style={{ width: "100%", height: "100%" }}
+              className="d-flex justify-content-center align-items-center"
+            >
+              <div style={{ padding: "50px" }}>
+                <p className="fw-bolder">No matching reward items found.</p>
+              </div>
+            </div>
+          )}
+        </>
+      )}
+    </main>
   );
 };
 
-export const useCart = () => useContext(CartContext);
-    TypeError: Cannot destructure property 'state' of '((cov_s90ywmcz3(...).s[9]++) , (0 , _CartContext.useCart)(...))' as it is null.
- 
-      28 |   const [pointsCost, setPointsCost] = useState(0);
-      29 |   const [isProcessing, setIsProcessing] = useState(false);
-> 30 |   const {state, dispatch} = useCart();
+export default RewardsSelectionPage;
+
+
+
+make the sort feature work for A-Z, Z-A, most expensive, least expensive
+
+rewardItem: 
+id
+: 
+1
+imgSrc
+: 
+"giftcard1"
+itemCost
+: 
+50
+itemDescription
+: 
+"Use your points to shop at Amazon"
+itemName
+: 
+"Amazon"
+rewardCatalogId
+: 
+1
+rewardsMessage
+: 
+undefined
+status
+: 
+"Inactive"
