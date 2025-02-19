cb4acb5882d2aaff0af3b30b91e5e648cdbcdf6b
Ayush-Dhawan <122392703+Ayush-Dhawan@users.noreply.github.com>
2025-02-19 22:11:30 +0530

    Update scripts.js

--- js/scripts.js
+++ js/scripts.js
@@ -10,3 +10,11 @@ Unable to understand why this doesnt work
 
 i want to get top 10 most fulfilled items, this can be obained by checking the total number of times a reward_catalog_item has been fulfilled using reward_catalog_item_id field in the fulfillment table
 i want to return entire reward catalog items for those 10 most fulfilled ones
+<select id="getTop10FulfillmentsByRewardItem" resultType="com.jpmorgan.et.model.RewardItem">
+    SELECT rci.id, rci.item_name, rci.img_src, rci.item_cost, rci.item_description, COUNT(f.reward_catalog_item_id) AS occurrence
+    FROM rewards_catalog_items rci
+    LEFT JOIN fulfillment f ON rci.id = f.reward_catalog_item_id
+    GROUP BY rci.id, rci.item_name, rci.img_src, rci.item_cost, rci.item_description
+    ORDER BY occurrence DESC
+    LIMIT 10;
+</select>
